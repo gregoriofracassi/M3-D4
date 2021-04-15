006e4f8d3ceaf0e8619ee62f83ef0a23cc56d3c6
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-04-15 17:02:21 +0200

    changes

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ fetch('https://striveschool-api.herokuapp.com/books')
 .then(response => response.json())
 .then(res => {
     displayBooks(res)
+    assignCart()
 })
+
+const cart = []
+
 
 const displayBooks = (books) => {
     books.forEach ( book => {
@@ -14,10 +18,32 @@ const displayBooks = (books) => {
           <img class="card-img-top" src="${book.img}" height="250" alt="Card image cap">
           <div class="card-body">
             <p class="card-title">${book.title}</p>
-            <a href="#" class="btn btn-sm btn-secondary">Add to cart</a>
+            <a href="#" class="btn cart-btn btn-sm btn-secondary">Add to cart</a>
           </div>
         </div>
         `
         row.appendChild(bookDiv)
     })
 }
+
+const addToCart = (e) => {
+    let cartBtn = e.currentTarget
+    let cardDiv = cartBtn.closest('.card')
+    if (cartBtn.dataset.status !== 'remove') {
+        cardDiv.style.border = 'solid 2px aquamarine'
+        cartBtn.innerHTML = 'Remove'
+        cartBtn.setAttribute('data-status', 'remove')
+    } else if (cartBtn.dataset.status === 'remove'){
+        cardDiv.style.border = '0px'
+        cartBtn.innerHTML = 'Add to cart'
+        cartBtn.dataset.status = 'add'
+    }
+    console.log(cardDiv.querySelector('.card-title').innerHTML)
+}
+
+const assignCart = () => {
+    const cartBtn = document.querySelectorAll('.cart-btn')
+    cartBtn.forEach(btn => btn.onclick = addToCart)
+}
+
+
