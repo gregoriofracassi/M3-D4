44941959db989becf6f64485ae7e0cef257bb847
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-04-15 18:38:47 +0200

    debugged addtocart

--- script.js
+++ script.js
@@ -16,28 +16,28 @@ fetch('https://striveschool-api.herokuapp.com/books')
     //     // console.log(cart)
     // }
     
-    const displayBooks = (books) => {
-        books.forEach ( book => {
-            const row = document.querySelector('.main-list')
-            const bookDiv = document.createElement('div')
-            bookDiv.classList.add('col-2')
-            bookDiv.innerHTML = `
-            <div class="card">
-            <img class="card-img-top" src="${book.img}" height="250" alt="Card image cap">
-            <div class="card-body">
-            <p class="card-title">${book.title}</p>
-            <a href="#" class="btn cart-btn btn-sm btn-secondary">Add to cart</a>
-            </div>
-            </div>
-            `
-            row.appendChild(bookDiv)
-        })
-    }
+const displayBooks = (books) => {
+    books.forEach ( book => {
+        const row = document.querySelector('.main-list')
+        const bookDiv = document.createElement('div')
+        bookDiv.classList.add('col-2')
+        bookDiv.innerHTML = `
+        <div class="card">
+        <img class="card-img-top" src="${book.img}" height="250" alt="Card image cap">
+        <div class="card-body">
+        <p class="card-title">${book.title}</p>
+        <a href="#" class="btn cart-btn btn-sm btn-secondary">Add to cart</a>
+        </div>
+        </div>
+        `
+        row.appendChild(bookDiv)
+    })
+}
     
 const sendToCart = (str) => {
         console.log('argument of the function:  ', str)
         console.log('cart before push', cart)
-        let cartedBook = allBooks.find(b => b.title = str)
+        let cartedBook = allBooks.find(b => b.title === str)
         console.log('book clicked', cartedBook)
         cart.push(cartedBook)
         console.log('cart', cart)
